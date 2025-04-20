34e213a67bf557fabf6c3679baafe1026e12702f
Jeremiah Deku <jdeku573@gmail.com>
2025-04-20 11:33:35 +0000

    another

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
+import { defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules/react')) {
+            return 'vendor-react';
+          }
+          if (id.includes('node_modules/lodash')) {
+            return 'vendor-lodash';
+          }
+          // fallback: let Vite handle other chunks
+        }
+      }
+    }
+  }
 })
