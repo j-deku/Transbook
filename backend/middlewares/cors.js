d03272ae03d76710c4c6a6e11081df68f9ad1fcd
Jeremiah Deku <jdeku573@gmail.com>
2025-04-21 06:57:32 +0000

    new changes 2

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -7,19 +7,6 @@ const corsOptions = {
   origin: [process.env.FRONTEND_URL, process.env.ADMIN_URL, process.env.DRIVER_URL], 
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   credentials: true,
-  allowedHeaders: [
-    'Content-Type',
-    'Authorization',
-    'X-Requested-With',
-    'Accept',
-    'Origin',
-    'X-CSRF-Token',
-    'X-HTTP-Method-Override',
-    'X-HTTP-Method',
-    'X-HTTP-Method-Override',
-    'application/json',
-    'application/javascript',
-  ]
 };
 
 const corsMiddleware = cors(corsOptions);
